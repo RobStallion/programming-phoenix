340609853f9ee549cb82109c5c1684613c99d1e3
Robert Francis <robertefrancis18@gmail.com>
2019-03-18 18:26:56 +0000

    links user accounts with credentials table

--- lib/rumbl/accounts/credential.ex
+++ lib/rumbl/accounts/credential.ex
@@ -4,8 +4,9 @@ defmodule Rumbl.Accounts.Credential do
 
   schema "credentials" do
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
-    field :user_id, :id
+    belongs_to :user, Rumbl.Accounts.User
 
     timestamps()
   end
@@ -13,8 +14,20 @@ defmodule Rumbl.Accounts.Credential do
   @doc false
   def changeset(credential, attrs) do
     credential
-    |> cast(attrs, [:email, :password_hash])
-    |> validate_required([:email, :password_hash])
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_length(:password, min: 6, max: 100)
     |> unique_constraint(:email)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
