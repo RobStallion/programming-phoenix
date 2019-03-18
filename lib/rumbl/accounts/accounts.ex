b3ea461700750afbc8d33cb022e1cc282ce5d3e1
Robert Francis <robertefrancis18@gmail.com>
2019-03-18 12:28:51 +0000

    update accounts context

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -4,22 +4,21 @@ defmodule Rumbl.Accounts do
   """
 
   alias Rumbl.Accounts.User
-
-  def list_users do
-    [
-      %User{id: "1", name: "José", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
-  end
+  alias Rumbl.Repo
 
   def get_user(id) do
-    Enum.find(list_users(), &(&1.id == id))
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn(map) ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
   end
 end
